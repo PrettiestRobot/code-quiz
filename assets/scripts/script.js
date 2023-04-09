5e46a3d458e5010247476c428f77245c58f266a0
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-04-08 23:00:59 -0400

    body content started

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,14 @@
+var questionContent = document.querySelector(".question");
+var answersContent = document.querySelector(".answers");
+var responseContent = document.querySelector("response");
+var timerContent = document.querySelector(".timer");
+
+function quizDefault() {
+    timerContent.textContent = "00";
+    questionContent.textContent = "Welcome to the Code quiz! Answer all the questions before the time runs out and beat the high score! Incorrect answers subtract points. Correct andswers add points. Good luck!"
+    
+}
+
+quizDefault();
 
 
