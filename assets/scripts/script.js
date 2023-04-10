2e0b94b8316f81d0a05b38b8e452673aabd45f02
Thomas Rose-Meyer <prettiest.robot@gmail.com>
2023-04-09 20:12:45 -0400

    basic body layout and default js finished

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,62 @@
 var questionContent = document.querySelector(".question");
-var answersContent = document.querySelector(".answers");
-var responseContent = document.querySelector("response");
+var answersContent = document.querySelector(".answers-option");
+var responseContent = document.querySelector(".response");
 var timerContent = document.querySelector(".timer");
+var startBtn = document.querySelector(".start-btn");
+var defaultCont = document.querySelector(".default-container");
+var quizCont = document.querySelector(".quiz-container");
+
+startBtn.addEventListener('click', startQuiz)
+
+const questions = [
+    {
+        question: 'Arrays in JavaScript can be used to store _____.',
+        answer: [
+            {text: 'Numbers', correct: false},
+            {text: 'Strings', correct: false},
+            {text: 'Objects', correct: false},
+            {text: 'All of the above', correct: true},
+        ]
+    },
+    {
+        question: 'In Math.random() what best describes .random() at its most basic?',
+        answer: [
+            {text: 'a method', correct: true},
+            {text: 'an object', correct: false},
+            {text: 'a property', correct: false},
+            {text: 'a d&d rule set', correct: false},
+        ]
+    },
+]
+
+
 
 function quizDefault() {
     timerContent.textContent = "00";
-    questionContent.textContent = "Welcome to the Code quiz! Answer all the questions before the time runs out and beat the high score! Incorrect answers subtract points. Correct andswers add points. Good luck!"
-    
+    if(!responseContent.classList.contains('hidden')){
+        responseContent.classList.add('hidden');
+    }
+    if(!quizCont.classList.contains('hidden')){
+        quizCont.classList.add('hidden');
+    }
+    if(defaultCont.classList.contains('hidden')){
+        defaultCont.classList.remove('hidden');
+    }
 }
-
 quizDefault();
 
 
+function startQuiz() {
+    defaultCont.classList.add("hidden");
+    if(quizCont.classList.contains('hidden')){
+        quizCont.classList.remove('hidden');
+    }
+}
+
+function setNextQuestion() {
+    
+}
+
+function selectAnswer() {
+
+}
